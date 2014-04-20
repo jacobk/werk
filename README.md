738a9d0eaecc0c08e4ef5f110e59a27d320b1925
jacob.kristhammar <jacob.kristhammar@mblox.com>
2014-04-20 22:42:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+## TODO
+
+* werk new (new PR)
+* werk add (add task)
+* werk team (who short status for all open PRs)
+* werk comment (add comment to current PR)
+* werk snap (comment with screenshot)
+* werk open (open browser)
+* werk info (show PR description)
+
+## Development
+
+1. `git@github.com:jacobk/werk-test.git`
+2. `werk-test`
+3. `npm link werk`
+4. `npm install`
+
+
+## Ideas
+
+* Find tasks before committing
+* PR KPI (detect if not enough activity, commits, comments, etc)
+
+
+##  Resources
+
+https://www.npmjs.org/package/grunt-githooks
+https://www.npmjs.org/package/grunt-todo
+https://www.npmjs.org/package/open
---
+++ bin/werk.js
@@ -0,0 +1,68 @@
+#!/usr/bin/env node
+
+/*
+ * https://github.com/jacobk/werk
+ *
+ * Copyright (c) 2014 Jacob Kristhammar
+ * Licensed under the MIT license.
+ */
+
+'use strict';
+
+var _ = require('lodash'),
+    RSVP = require('rsvp'),
+    moment = require('moment'),
+    grunt = require('grunt'),
+    logger = require('../lib/logger'),
+    github = require('../lib/github'),
+    git = require('../lib/git'),
+    prompt = require('../lib/prompt'),
+    shell = require('../lib/shell'),
+    argv = require('minimist')(process.argv.slice(2)),
+    cmdName = argv._[0].toLowerCase(),
+    cmd;
+
+if (argv._.length === 0) {
+  logger.log.warn('USAGE');
+  process.exit(1);
+}
+
+try {
+  cmd = require('../lib/cmds/' + cmdName);
+} catch(e) {
+  logger.log.error('Unknown command: \'' + cmdName.underline + '\'');
+  process.exit(1);
+}
+
+RSVP.hash({
+  isDirty: git.isRepoDirty(),
+  branch: git.currentBranch(),
+  name: git.repoName(),
+  owner: git.repoOwner()
+}).then(function(repoInfo) {
+  logger.log.ok(cmd.shortDesc);
+  return github.withGitHubAuth().then(function() {
+    return cmd.run(repoInfo).then(function() {
+      logger.log.ok();
+    });
+  });
+}).catch(function(reason) {
+  if (!_.isUndefined(reason.help)) {
+    logger.log.fail(reason.reason);
+    logger.log.writeln(reason.help);
+  } else {
+    logger.log.error(reason);
+    logger.log.error(reason.stack);
+  }
+});
+
+
+// Try not to miss unhandled RSVP errors
+RSVP.on('error', function(reason) {
+  logger.log.error('RSVP error');
+  if (reason.stack) {
+    logger.log.error(reason.stack);
+  } else {
+    logger.log.error(JSON.stringify(reason));
+  }
+});
