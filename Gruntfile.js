d4b9125f28c328020a5216e627de97d4c29cce4b
jacob.kristhammar <jacob.kristhammar@mblox.com>
2014-06-05 14:19:40 +0200

    Remove legacy test stuff

--- Gruntfile.js
+++ Gruntfile.js
@@ -17,7 +17,6 @@ module.exports = function(grunt) {
         'Gruntfile.js',
         'bin/*.js',
         'lib/*.js',
-        '<%= nodeunit.tests %>',
       ],
       options: {
         jshintrc: '.jshintrc',
@@ -27,24 +26,14 @@ module.exports = function(grunt) {
     // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp'],
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js'],
     }
   });
 
   // These plugins provide necessary tasks.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-clean');
-  grunt.loadNpmTasks('grunt-contrib-nodeunit');
-
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'nodeunit']);
 
   // By default, lint and run all tests.
-  grunt.registerTask('default', ['jshint', 'test']);
+  grunt.registerTask('default', ['jshint']);
 
 };
